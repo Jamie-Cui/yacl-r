4004172111dfb9592a15f1c22f96bc9cfcb939f7
Jamie Cui <jamie.cui@outlook.com>
2024-11-22 19:40:16 +0800

    fix(ci): cleanup format and license

--- yacl/engine/plaintext/executor_test.cc
+++ yacl/engine/plaintext/executor_test.cc
@@ -1,5 +1,4 @@
-
-// Copyright 2024 Ant Group Co., Ltd.
+// Copyright 2024 Jamie Cui and Ant Group Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
