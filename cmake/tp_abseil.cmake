38372d0a7cbc0e33c939bcfc4ec1db8fb1133b93
Jamie <jamie.cui@outlook.com>
2024-12-24 17:15:41 +0800

    Update tp_abseil.cmake

--- cmake/tp_abseil.cmake
+++ cmake/tp_abseil.cmake
@@ -20,21 +20,79 @@ ExternalProject_Add(abseil
     SHA256=f50e5ac311a81382da7fa75b97310e4b9006474f9560ac46f54a9967f07d4ae3
   CMAKE_ARGS ${CMAKE_ARGS}
     -DCMAKE_INSTALL_PREFIX=${CMAKE_THIRDPARTY_PREFIX}
-    -DABSL_BUILD_MONOLITHIC_SHARED_LIBS=On
-    -DBUILD_SHARED_LIBS=On
+    # -DABSL_BUILD_MONOLITHIC_SHARED_LIBS=On
+    # -DBUILD_SHARED_LIBS=On
   PREFIX ${CMAKE_THIRDPARTY_PREFIX}
   LOG_DOWNLOAD On
   LOG_CONFIGURE On
   LOG_BUILD On
   LOG_INSTALL On)
 
-add_library(libabseil SHARED IMPORTED)
-set_property(
-  TARGET libabseil PROPERTY
-  IMPORTED_LOCATION ${CMAKE_THIRDPARTY_LIBDIR}/libabseil_dll${CMAKE_SHARED_LIBRARY_SUFFIX})
-add_dependencies(libabseil abseil)
+# ------------------------------------------------------------------------------
+# How to use absl?
+# ------------------------------------------------------------------------------
+
+# add_library(libabseil_dll SHARED IMPORTED)
+# set_property(TARGET libabseil_dll
+#     PROPERTY IMPORTED_LOCATION "${CMAKE_THRIDPARTY_LIB_DIRECOTRY}/libabseil_dll${CMAKE_SHARED_LIBRARY_SUFFIX}")
+# add_dependencies(libabseil_dll abseil-cpp)
+
+add_library(libabsl_interface INTERFACE)
+
+# HACK
+macro(import_absl_static_lib name)
+  add_library(${name} STATIC IMPORTED)
+  set_property(
+    TARGET ${name}
+    PROPERTY IMPORTED_LOCATION ${CMAKE_THRIDPARTY_LIB_DIRECOTRY}/${name}${CMAKE_STATIC_LIBRARY_SUFFIX})
+  add_dependencies(${name} abseil-cpp)
+  target_link_libraries(libabsl_interface
+    INTERFACE
+    ${name})
+endmacro()
+
+import_absl_static_lib(libabsl_raw_hash_set)
+import_absl_static_lib(libabsl_hash)
+import_absl_static_lib(libabsl_city)
+import_absl_static_lib(libabsl_low_level_hash)
+import_absl_static_lib(libabsl_random_internal_pool_urbg)
+import_absl_static_lib(libabsl_random_internal_randen)
+import_absl_static_lib(libabsl_random_internal_randen_hwaes)
+import_absl_static_lib(libabsl_random_internal_randen_hwaes_impl)
+import_absl_static_lib(libabsl_random_internal_randen_slow)
+import_absl_static_lib(libabsl_random_internal_platform)
+import_absl_static_lib(libabsl_random_internal_seed_material)
+import_absl_static_lib(libabsl_random_seed_gen_exception)
+import_absl_static_lib(libabsl_statusor)
+import_absl_static_lib(libabsl_status)
+import_absl_static_lib(libabsl_cord)
+import_absl_static_lib(libabsl_cordz_info)
+import_absl_static_lib(libabsl_cord_internal)
+import_absl_static_lib(libabsl_cordz_functions)
+import_absl_static_lib(libabsl_exponential_biased)
+import_absl_static_lib(libabsl_cordz_handle)
+import_absl_static_lib(libabsl_strerror)
+import_absl_static_lib(libabsl_str_format_internal)
+import_absl_static_lib(libabsl_synchronization)
+import_absl_static_lib(libabsl_stacktrace)
+import_absl_static_lib(libabsl_symbolize)
+import_absl_static_lib(libabsl_malloc_internal)
+import_absl_static_lib(libabsl_time)
+import_absl_static_lib(libabsl_strings)
+import_absl_static_lib(libabsl_strings_internal)
+import_absl_static_lib(libabsl_base)
+import_absl_static_lib(libabsl_spinlock_wait)
+import_absl_static_lib(libabsl_int128)
+import_absl_static_lib(libabsl_throw_delegate)
+import_absl_static_lib(libabsl_time_zone)
+import_absl_static_lib(libabsl_raw_logging_internal)
+import_absl_static_lib(libabsl_debugging_internal)
+import_absl_static_lib(libabsl_demangle_internal)
+import_absl_static_lib(libabsl_demangle_rust)
+import_absl_static_lib(libabsl_decode_rust_punycode)
+import_absl_static_lib(libabsl_utf8_for_code_point)
 
 # -----------------------------
 # Alias Target for External Use
 # -----------------------------
-add_library(Thirdparty::absl ALIAS libabseil)
+add_library(Thirdparty::absl ALIAS libabsl_interface)
