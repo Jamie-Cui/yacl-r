294b45a87f46366d49c8f6ac02007b81325aa582
Jamie Cui <jamie.cui@outlook.com>
2025-06-07 20:28:17 +0800

    fix

--- cmake/deps/fmt.cmake
+++ cmake/deps/fmt.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-externalproject_add(
+ExternalProject_Add(
   fmt
   URL https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.tar.gz
   URL_HASH
